6d3447ccad4ceb4815fb6e4917e3ea42b7861849
WolverinDEV <git@did.science>
2026-03-24 12:48:34 +0100

    feat: using tailwind 4.2 and fix automatic class generation

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, UserConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 import { resolveVersionHash } from '../../build/resolveVersionHash'
 
 export default defineConfig(({ isSsrBuild }) => ({
@@ -10,8 +11,9 @@ export default defineConfig(({ isSsrBuild }) => ({
         __APP_VERSION_HASH__: JSON.stringify(resolveVersionHash())
     },
     plugins: [
+        tailwindcss(),
         react(),
-        babel({ presets: [reactCompilerPreset()] })
+        babel({ presets: [reactCompilerPreset()] }),
     ],
     ssr: isSsrBuild ? { noExternal: true } : undefined,
     build: {
